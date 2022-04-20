ad202c1c11e4b0e57e13af50b443be99bd14a8d1
xiabee <xiabee@foxmail.com>
2022-04-20 17:24:03 +0800

    update

--- main.go
+++ main.go
@@ -3,14 +3,6 @@ package main
 import "main/audit"
 
 func main() {
-	// file := `test.txt`
-	// data := pre.Readlines(file)
-	//fmt.Println(data)
-	// for i := 0; i < len(data); i++ {
-	// 	audit.Error_based_detection(data[i])
-	// 	audit.Time_based_bool_detection(data[i])
-	// }
-	// 按切片解析输入
 	sql_file := `test.txt`
 	rules_dir := `./rules/sql_injection`
 	audit.Sql_injection_detection(sql_file, rules_dir)
